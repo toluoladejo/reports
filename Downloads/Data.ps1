88510eec233904a0ed92d6bc1d682128f50212df
Tolu Oladejo <you@example.com>
2024-04-12 13:18:52 -0700

    uuuuu

--- Downloads/Data.ps1
+++ Downloads/Data.ps1
@@ -1,54 +1,117 @@
 $domain = "desktop-3q0egib"
 $port = 80
-$ReportServerUri = "http://"+$domain+":"+$port+"/ReportServer/ReportService2010.asmx?wsdl"
+$SSRSWebServiceUrl = "http://"+$domain+":"+$port+"/ReportServer/ReportService2010.asmx?wsdl"
 $reportPath = "/sample1"
 $fileExtension = ".rdl"
-$reportDataSourcePath = "/sample1/DataSource1"  # Example data source path
+$warnings = $null
 
 $sourceFiles = "C:\Users\nessien\Documents\reports\"
 
-$reports = Get-ChildItem $sourceFiles -Filter *$fileExtension | % { $_.Name}
+$localReports = Get-ChildItem $sourceFiles -Filter *$fileExtension | % { $_.Name}
 
-# SSRS Web Service Proxy
-#$SSRSProxy = New-WebServiceProxy -Uri $ReportServerUri -Namespace SSRSWebService
+# Data Source Parameters
+$newDataSourcePath = "/MyServer"  # Reference to the data source in SSRS
+$newDataSourceName = "MyServer"  # Reference to the data source in SSRS
 
-# Authenticate (provide credentials if necessary)
-# For example:
-#$SSRSProxy.Credentials = New-Object System.Net.NetworkCredential("test-user", "ndimatess", "desktop-3q0egib")
+$UserName = "APPZONEGROUP\test-user"
+$Password = "ndimatess"
 
-function uploadReport{
-    param (
-        [string]$ReportServerUri,
-        [string]$ReportPath,
-        [string]$reportFile,
-        [boolean]$replace,
-        [string]$DataSourcePath
-    )
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($UserName, $SecurePassword)
 
-    if ($replace) {
-        Write-RsCatalogItem -ReportServerUri $ReportServerUri -Path $reportFile -RsFolder $ReportPath -Overwrite -DataSourcePath $DataSourcePath
-    } else {
-        Write-RsCatalogItem -ReportServerUri $ReportServerUri -Path $reportFile -RsFolder $ReportPath -DataSourcePath $DataSourcePath
-    }
+# SSRS Web Service Proxy Authentication
+$SSRSProxy =  New-WebServiceProxy -Uri $SSRSWebServiceUrl -Credential $Credential -Namespace "SSRS"
 
-    
-}
+#Remote report files 
+$remoteReports = $SSRSProxy.ListChildren($reportPath,$true) | % { $_.Name+$fileExtension} | Out-String
 
-$list = Get-RsFolderContent -ReportServerUri $ReportServerUri -RsFolder $reportPath | % { $_.Name+$fileExtension} | Out-String
 
-$reports | ForEach-Object {
+$localReports | ForEach-Object {
+
     
     $file = $sourceFiles+$_
 
-    # Check if "file" exists in the list
-    if ($list -match $_) {
-        Write-Host $_" already exist in the report path: $reportPath"
-        echo "Replacing report with new one........"
-        uploadReport -ReportServerUri $ReportServerUri -ReportPath $reportPath -reportFile $file -replace $true -DataSourcePath $reportDataSourcePath
-        echo "Done replacing old report!"
-    } else {
-        # upload
-        uploadReport -ReportServerUri $ReportServerUri -ReportPath $reportPath -reportFile $file -replace $false -DataSourcePath $reportDataSourcePath
-      # echo $file #| Out-String
+    $reportName = [System.IO.Path]::GetFileNameWithoutExtension($_)
+    $bytes = [System.IO.File]::ReadAllBytes($file)
+
+    if ($remoteReports -match $_) {
+        $cj = "with replacement"
+    }else{
+        $cj = ""
+    }
+
+    Write-Host "Uploading "$cj" of "$_" report in progress..."
+
+    $report = $SSRSProxy.CreateCatalogItem(
+        "Report",         # Catalog item type
+        $reportName,      # Report name
+        $reportPath,      # Destination folder
+        $true,            # Overwrite report if it exists?
+        $bytes,           # .rdl file contents
+        $null,            # Properties to set.
+        [ref]$warnings    # Warnings that occured while uploading.
+    )   
+
+    Write-Output "Upolading process completed!"
+
+    $warnings | ForEach-Object {
+        #Write-Output ("Warning: {0}" -f $_.Message)
+    }
+
+    Write-Host "Connecting "$_" report to "$newDataSourcePath" data source....."
+
+
+    $referencedDataSourceName = (@($SSRSProxy.GetItemReferences($report.Path, "DataSource")))[0].Name
+
+    if ($referencedDataSourceName.Length -gt 0){
+        Write-Host "Connecting in progress....."
+        Set-RsDataSourceReference -ReportServerUri $SSRSWebServiceUrl -Path $report.path  -DataSourceName $referencedDataSourceName -DataSourcePath $newDataSourcePath -Verbose
+        if ($?) {
+            Write-Host "Successfully connected "$report.path" report to $newDataSourcePath datasource`n"
+        }else{
+            Write-Warning "Error occured while connecting "$report.path" report to $newDataSourcePath datasource`n"
+        }       
+    }else{
+
+        Write-Warning $_" report has no initial datasource, connecting to "$newDataSourcePath" datasource ...."
+
+        #$dataSources = Get-RsItemDataSource -RsItem $report.Path
+        #$dataSources.Name = $newDataSourcePath
+        #$dataSources.Item = New-Object SSRS.DataSourceReference
+        #$dataSources.Item.Reference = $newDataSourcePath 
+
+        #$dataSource = New-Object SSRS.DataSource
+        #$dataSource.Name = $newDataSourceName      # Name as used when designing the Report
+        #$dataSource.Item = New-Object SSRS.DataSourceReference
+        #$dataSource.Item.Reference = $newDataSourcePath # Path to the shared data source as it is d
+        
+        #Set-RsItemDataSource -RsItem $report.Path -DataSource $dataSource
+
+        #Set-RsDataSource -RsItem $report.Path -DataSourceDefinition $dataSources -ErrorAction SilentlyContinue
+        
+        #if ($?) {
+         #   Write-Host "Successfully connected "$report.path" report to $newDataSourcePath datasource`n"
+        #}else{
+         #   Write-Warning "Error occured while connecting "$report.path" report to $newDataSourcePath datasource`n"
+        #}   
     }
 }
+
+
+
+
+
+
+
+
+
+# Upload the file
+#$UploadResult = $SSRSProxy.CreateCatalogItem("Report", $ReportPath, $FileName, $null, $null, $FileContent, [ref]$null, [ref]$null, $DataSourceReference)
+
+# Check if upload was successful
+#if ($UploadResult -eq $null) {
+ #   Write-Host "File upload failed."
+#} else {
+#    Write-Host "File uploaded successfully."
+#}
+
